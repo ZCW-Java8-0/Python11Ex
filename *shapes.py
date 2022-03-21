0e94eda6985f3094260ac042e0e43b531e1149a8
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-21 11:55:20 -0400

    Finished exercise1, created base and subclass.

--- *shapes.py
+++ *shapes.py
@@ -5,19 +5,21 @@ class Rectangle:
     def __init__(self):
         pass
 
-def area():
+    def area_of(self, length, width):
+        area = length * width
+        return area
 
-
-
-
-
-def perimeter():
-
-
-
-
+    def perimeter_of(self, length, width):
+        perimeter = 2 * (length + width)
+        return perimeter
 
 
 class Square(Rectangle):
     def __init__(self):
         pass
+
+
+square1 = Square()
+square_area = square1.area_of(2, 3)
+print(f'The area of square1 is: {square_area}')
+
