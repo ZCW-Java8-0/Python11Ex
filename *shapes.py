ab017fbeefff48c3c072afb8966606e2801ae20c
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-21 11:15:49 -0400

    Set up Classes.

---
+++ *shapes.py
@@ -0,0 +1,23 @@
+class Rectangle:
+    length = 0
+    width = 0
+
+    def __init__(self):
+        pass
+
+def area():
+
+
+
+
+
+def perimeter():
+
+
+
+
+
+
+class Square(Rectangle):
+    def __init__(self):
+        pass
