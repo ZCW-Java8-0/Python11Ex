f227882d5685dfacd119511017e39e4d8c453924
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-21 20:10:04 -0400

    Made some changes to Instructor class and methods.

--- gradebook.py
+++ gradebook.py
@@ -1,5 +1,5 @@
 from enum import Enum
-import uuid
+from uuid import uuid4
 
 
 class AliveStatus(Enum):
@@ -7,7 +7,7 @@ class AliveStatus(Enum):
     ALIVE = 1
 
 
-class Person():
+class Person:
     first_name = ''
     last_name = ''
     dob = ''
@@ -16,9 +16,9 @@ class Person():
     def __init__(self):
         pass
 
-    def update_first_name(self, new_first_name):
-        self.first_name = new_first_name
-        return new_first_name
+    def update_first_name(self, first_name):
+        self.first_name = first_name
+        return first_name
 
     def update_last_name(self, new_last_name):
         self.last_name = new_last_name
@@ -34,29 +34,28 @@ class Person():
 
 
 class Instructor(Person):
-    instructor_id = uuid.uuid4()
+    instructor_id = uuid4()
 
-    def __init__(self):
-        pass
+    def __init__(self, first_name, last_name, dob, alive):
+        super().__init__()
 
 
 class Student(Person):
-    student_id = uuid.uuid4()
+    student_id = uuid4
 
-    def __init__(self):
-        pass
+    def __init__(self, first_name, last_name, dob, alive):
+        super().__init__()
 
 
 class ZipCodeStudent(Student):
 
-    def __init__(self):
-        pass
+    def __init__(self, first_name, last_name, dob, alive):
+        super().__init__()
 
 
 class College(Student):
-
-    def __init__(self):
-        pass
+    def __init__(self, first_name, last_name, dob, alive):
+        super().__init__()
 
 
 class Classroom:
@@ -66,6 +65,30 @@ class Classroom:
     def __init__(self):
         pass
 
+    def add_instructor(self, instructor):
+        self.instructors.append(instructor.instructor_id)
+        return self.instructors
 
-student1 = Student()
-print(student1.student_id)
+    def remove_instructor(self):
+        return self.instructors
+
+    def add_student(self):
+        return self.students
+
+    def remove_student(self):
+        return self.students
+
+    def print_instructors(self):
+        print(f'Instructor: {self.instructors}')
+
+    def print_students(self):
+        print(self.students)
+
+
+instructor1 = Instructor('Allen', 'Chung', '9/27/91', 1)
+
+print(instructor1.update_first_name('Allen'))
+
+classroom1 = Classroom()
+classroom1.add_instructor(instructor1)
+print(classroom1.print_instructors())
